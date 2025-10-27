34704e998cc9d68325142f189583aecfe4d65429
huzaifaquershi66 <165478101+huzaifaquershi66@users.noreply.github.com>
2025-10-27 14:34:36 +0500

    Update Trade.jsx

--- src/components/Trade.jsx
+++ src/components/Trade.jsx
@@ -62,7 +62,7 @@ const TradePage = () => {
   const updateDeposit = async (userId, currency, amount) => {
     try {
       await axios.post(
-        "https://gainti.work.gd/api/auth/updateDeposit",
+        "https://casino2-e0vk.onrender.com/api/auth/updateDeposit",
         {
           userId,
           currency,
